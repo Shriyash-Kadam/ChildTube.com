b53a45bd12429e0d5089da6ac846374063ea6034
Shriyash-Kadam <126370082+Shriyash-Kadam@users.noreply.github.com>
2023-05-07 17:51:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+
+function addUser() {
+      window.location = "story.html";
+  }
+
+  function music() {
+    window.location = "music.html";
+}
+
+function cartoons() {
+    window.location = "cartoon.html";
+}
+
+function play1() {
+    window.location.href = "https://www.youtube.com/@PeppaPigOfficial/videos";
+}
+
+function play3() {
+    window.location.href = "https://www.youtube.com/@MashaBearEN/videos";
+}
+
+function play2() {
+    window.location.href = "https://www.youtube.com/@MrBean/videos";
+}
+
+function play4() {
+    window.location.href = "https://www.youtube.com/@WolfooFamily/videos";
+}
+
+
